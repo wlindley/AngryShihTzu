947e6f957485d18874e68d924557b5f333541a05
wlindley <walker@walkerlindley.com>
2014-11-26 17:16:44 -0800

    Showing random dog image.

--- Assets/Scripts/Game/SpawnCommand.cs
+++ Assets/Scripts/Game/SpawnCommand.cs
@@ -1,5 +1,6 @@
 ﻿using strange.extensions.command.impl;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace AST.Game
 {
@@ -10,9 +11,20 @@ namespace AST.Game
 
         public override void Execute()
         {
-            var prefab = Resources.Load<GameObject>("Prefabs/DogImage");
-            var obj = GameObject.Instantiate(prefab) as GameObject;
+            var obj = InstantiatePrefab();
+            SetRandomImage(obj.GetComponent<Image>());
             reparentSignal.Dispatch(obj);
+        }
+
+        private static GameObject InstantiatePrefab()
+        {
+            var prefab = Resources.Load<GameObject>("Prefabs/DogImage");
+            return GameObject.Instantiate(prefab) as GameObject;
+        }
+
+        private void SetRandomImage(Image image)
+        {
+            image.sprite = Resources.Load<Sprite>("Images/" + Random.Range(0, 16).ToString("D2"));
         }
     }
 }
