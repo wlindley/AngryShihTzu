46e02f6957f40c1792132f5b08723848ab434e7b
wlindley <walker@walkerlindley.com>
2015-07-20 20:26:03 -0700

    Initial check in of polymorphic lists
    Code's not great yet and not all the features are there, but it's a
    decent first pass.

---
+++ Assets/Scripts/Editor/SceneAssetInspector.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+
+namespace AST
+{
+    public class SceneAssetInspector : AssetInspector
+    {
+        public void OnEnable(DefaultAsset target)
+        {
+            
+        }
+
+        public void OnInspectorGUI(DefaultAsset target)
+        {
+            EditorGUILayout.LabelField(target.name);
+        }
+
+        public bool IsValid(DefaultAsset target)
+        {
+            return AssetDatabase.GetAssetPath(target).EndsWith(".unity");
+        }
+    }
+}
