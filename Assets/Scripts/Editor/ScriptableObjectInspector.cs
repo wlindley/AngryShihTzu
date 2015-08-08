f2b369f430d4ea7098bad9497b3da409bba0a5bd
wlindley <walker@walkerlindley.com>
2015-08-07 22:36:38 -0700

    Adding back button and name field to ScriptableObject's editor.

--- Assets/Scripts/Editor/ScriptableObjectInspector.cs
+++ Assets/Scripts/Editor/ScriptableObjectInspector.cs
@@ -1,21 +1,37 @@
 ﻿using UnityEngine;
 using UnityEditor;
-using System;
 
 namespace AST
 {
-    [CustomEditor(typeof(MonoScript))]
+    [CustomEditor(typeof(ScriptableObject), true)]
     public class ScriptableObjectInspector : Editor
     {
-        private ScriptableObjectHelper helper = ScriptableObjectHelper.GetInstance();
-
         public override void OnInspectorGUI()
         {
-            var type = (target as MonoScript).GetClass();
-            if (!helper.IsTypeScriptableObject(type))
-                DrawDefaultInspector();
-            else if (GUILayout.Button("Create Instance"))
-                helper.CreateScriptableObjectInstance(type);
+            DrawBackButton();
+            DrawNameField();
+            DrawDefaultInspector();
+        }
+
+        private void DrawNameField()
+        {
+            name = EditorGUILayout.TextField("Name", name);
+        }
+
+        private void DrawBackButton()
+        {
+            if (IsSubAsset() && GUILayout.Button("◄ Back to Parent"))
+                AssetDatabase.OpenAsset(GetParent());
+        }
+
+        private bool IsSubAsset()
+        {
+            return GetParent() != target;
+        }
+
+        private ScriptableObject GetParent()
+        {
+            return AssetDatabase.LoadAssetAtPath<ScriptableObject>(AssetDatabase.GetAssetPath(target));
         }
     }
 }
